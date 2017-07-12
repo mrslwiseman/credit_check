523b9b7883e4a323d9a84ef82fb96099eb1f9437
Matt Wiseman <mrslwiseman@gmail.com>
2017-07-12 14:40:44 +1000

    refactor check_sum conditionals

--- credit_check.rb
+++ credit_check.rb
@@ -44,19 +44,14 @@ private
   def check_sum(num_str, i = 0, sum = 0)
     return 0 if i >= num_str.length
     x = num_str[i].to_i
-    if (i % 2 == 0)
-      sum = x
-    else
-      if (x * 2) > 9
-        sum = (x * 2 - 9)
-      else
-        sum = x * 2
-      end
-    end
-    return sum += check_sum(num_str, i + 1, sum)
+    sum = if (i.even?)
+            x
+          elsif (x * 2) > 9
+            x * 2 - 9
+          else
+            x * 2
+          end
+    sum + check_sum(num_str, i + 1, sum)
   end
 
 end
-
-
-require_relative "credit_check_test"
